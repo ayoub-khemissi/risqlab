9fc9c2016983d68195691e473fe730cf37151e6f
Ayoub KHEMISSI <khemayoub@gmail.com>
2025-10-28 21:43:47 +0100

    feat: configure image optimization
    - Allow images from coinmarketcap

--- risqlab-front/next.config.js
+++ risqlab-front/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 's2.coinmarketcap.com',
+                pathname: '/static/img/coins/**',
+            },
+        ],
+    },
+};
 
 module.exports = nextConfig;
